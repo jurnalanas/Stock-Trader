1ef01f5d2cd1f0d656976afe0e682ec7fb908523
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2016-12-20 17:41:20 +0700

    populate props and disable binding

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
-          NAME
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
       <div class="panel-body">
@@ -12,14 +12,40 @@
           <input
             class="form-control"
             placeholder="Quantity"
+            v-model="quantity"
             type="number">
         </div>
         <div class="pull-right">
           <button
             class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
           >Buy</button>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data () {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock () {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        }
+
+        console.log(order)
+      }
+    }
+  }
+
+</script>
