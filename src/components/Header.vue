3e834997f34334b7cb5384e8e0d3c8421f203614
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2016-12-22 06:16:17 +0700

    added dropdown menu functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,10 @@
         <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#" @click="endDay">End Day</a></li>
-          <li class="dropdown">
+          <li
+            class="dropdown"
+            @click="isDropdownOpen = !isDropdownOpen "
+            :class="{open: isDropdownOpen}">
             <a
             href="#"
             class="dropdown-toggle"
@@ -36,6 +39,11 @@
   import {mapActions} from 'vuex'
 
   export default {
+    data () {
+      return {
+        isDropdownOpen: false
+      }
+    },
     computed: {
       funds () {
         return this.$store.getters.funds
