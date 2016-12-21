9b3d8dd08d996edb25b4012d68f11717dd1ee645
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2016-12-22 06:47:29 +0700

    added fetch data actions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,7 +26,7 @@
             aria-expanded="false">Save & Load <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="#" @click="saveData">Save Data</a></li>
-              <li><a href="#">Load Data</a></li>
+              <li><a href="#" @click="loadData">Load Data</a></li>
             </ul>
           </li>
         </ul>
@@ -50,9 +50,10 @@
       }
     },
     methods: {
-      ...mapActions([
-        'randomizeStocks'
-      ]),
+      ...mapActions({
+        randomizeStocks: 'randomizeStocks',
+        fetchData: 'loadData'
+      }),
       endDay () {
         this.randomizeStocks()
       },
@@ -63,6 +64,9 @@
           stocks: this.$store.getters.stocks
         }
         this.$http.put('data.json', data)
+      },
+      loadData () {
+        this.fetchData()
       }
     }
   }
